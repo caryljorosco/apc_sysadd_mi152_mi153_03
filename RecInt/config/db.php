ae816a06b4d03fc6da07ea5e5c65c95001ae6e8a
Gabriel Angelo A. Ngceen <gngceen@gmail.com>
2017-08-11 00:01:57 +0800

    configured db.php

--- RecInt/config/db.php
+++ RecInt/config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=sample',
+    'dsn' => 'mysql:host=localhost;dbname=spareservation',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
