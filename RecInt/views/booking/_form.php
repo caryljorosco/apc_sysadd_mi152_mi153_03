aea6f5bc9d114b0025be295260955c8b6c593ed9
Edward Joseph R. Mandac <edward.mandac@gmail.com>
2017-08-30 00:23:42 +0800

    booking crud

---
+++ RecInt/views/booking/_form.php
@@ -0,0 +1,37 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Booking */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="booking-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'Booking_Type')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'time_start')->textInput() ?>
+
+    <?= $form->field($model, 'time_end')->textInput() ?>
+
+    <?= $form->field($model, 'date_received')->textInput() ?>
+
+    <?= $form->field($model, 'duration')->textInput() ?>
+
+    <?= $form->field($model, 'Rooms_ID')->textInput() ?>
+
+    <?= $form->field($model, 'Customer_ID')->textInput() ?>
+
+    <?= $form->field($model, 'Employee_ID')->textInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
