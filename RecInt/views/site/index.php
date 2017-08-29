4809eadaa4a7708e66a64b586e14eb422752b083
Edward Joseph R. Mandac <edward.mandac@gmail.com>
2017-08-29 12:58:05 +0800

    edited

--- RecInt/views/site/index.php
+++ RecInt/views/site/index.php
@@ -36,14 +36,14 @@ $this->title = 'Reservation System (Asmara Spa)';
                 <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">View &raquo;</a></p>
             </div>
             <div class="col-lg-4">
-                <h2>Add a Customer</h2>
+                <h2>Create a Report</h2>
 
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
                     dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
                     ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
                     fugiat nulla pariatur.</p>
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Add &raquo;</a></p>
+                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Create &raquo;</a></p>
             </div>
         </div>
 
