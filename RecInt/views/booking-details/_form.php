edfdfc99949c14f458df9c4c9160158a6198856c
Edward Joseph R. Mandac <edward.mandac@gmail.com>
2017-08-30 00:25:16 +0800

    added booking details crud

---
+++ RecInt/views/booking-details/_form.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\BookingDetails */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="booking-details-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'Booking_ID')->textInput() ?>
+
+    <?= $form->field($model, 'Employee_ID')->textInput() ?>
+
+    <?= $form->field($model, 'Services_ID')->textInput() ?>
+
+    <?= $form->field($model, 'Rooms_ID')->textInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
