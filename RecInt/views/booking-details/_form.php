d489cd0641a0a2c9235768d25ad739b72d94d0d6
Edward Joseph R. Mandac <edward.mandac@gmail.com>
2017-08-30 03:13:53 +0800

    added more dropDownList to booking-details/_form.php

--- RecInt/views/booking-details/_form.php
+++ RecInt/views/booking-details/_form.php
@@ -4,6 +4,8 @@ use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 use yii\helpers\ArrayHelper;
 use app\models\Employee;
+use app\models\Services;
+use app\models\Rooms;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\BookingDetails */
@@ -17,15 +19,16 @@ use app\models\Employee;
     <?= $form->field($model, 'Booking_ID')->textInput() ?>
 
 	<?= $form->field($model, 'Employee_ID')->dropDownList(
-	ArrayHelper::map(Employee::find()->asArray()->all(),'ID',function($model, $defaultValue){
+	ArrayHelper::map(Employee::find()->asArray()->all(), 'ID',function($model, $defaultValue){
 			return $model['First_Name'].' '.$model['Last_Name'];}
 			,'Position')
 		)?>
 
-    <?= $form->field($model, 'Services_ID')->textInput() ?>
-
-    <?= $form->field($model, 'Rooms_ID')->textInput() ?>
-
+	<?= $form->field($model, 'Services_ID')->dropDownList(
+	ArrayHelper::map(Services::find()->all(),'ID','Service_Name','Price'))?>
+	
+	<?= $form->field($model, 'Rooms_ID')->dropDownList(
+	ArrayHelper::map(Rooms::find()->all(),'ID','Room_Number'))?>
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
     </div>
