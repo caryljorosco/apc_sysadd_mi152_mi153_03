06c50c5ef9e89d6b7859b185fc22b670780ddb25
Edward Joseph R. Mandac <edward.mandac@gmail.com>
2017-08-30 00:19:26 +0800

    added models/Customer.php

---
+++ RecInt/models/Customer.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "customer".
+ *
+ * @property integer $ID
+ * @property string $Last_Name
+ * @property string $First_Name
+ * @property string $Contact_Number
+ *
+ * @property Booking[] $bookings
+ */
+class Customer extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'customer';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['Last_Name', 'First_Name', 'Contact_Number'], 'required'],
+            [['Last_Name', 'First_Name'], 'string', 'max' => 45],
+            [['Contact_Number'], 'string', 'max' => 15],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'ID' => 'ID',
+            'Last_Name' => 'Last  Name',
+            'First_Name' => 'First  Name',
+            'Contact_Number' => 'Contact  Number',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getBookings()
+    {
+        return $this->hasMany(Booking::className(), ['Customer_ID' => 'ID']);
+    }
+}
