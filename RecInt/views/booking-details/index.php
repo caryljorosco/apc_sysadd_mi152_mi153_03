7e3bef6bb779082a97df212c0d82f582248624f3
Gabriel Angelo A. Ngceen <gngceen@gmail.com>
2017-08-29 14:48:20 +0800

    updated index.php

--- RecInt/views/booking-details/index.php
+++ RecInt/views/booking-details/index.php
@@ -24,7 +24,6 @@ $this->params['breadcrumbs'][] = $this->title;
         'columns' => [
             ['class' => 'yii\grid\SerialColumn'],
 
-            'ID',
             'Booking_ID',
             'Employee_ID',
             'Services_ID',
