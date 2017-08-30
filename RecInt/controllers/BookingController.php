ef9e62846ab044acbf6e3afa0b45461fbd6fe462
Giomar Andres <cloudaxx@gmail.com>
2017-08-30 11:58:19 +0800

    Created BookingController

--- RecInt/controllers/BookingController.php
+++ RecInt/controllers/BookingController.php
@@ -47,12 +47,15 @@ class BookingController extends Controller
     /**
      * Displays a single Booking model.
      * @param integer $id
+     * @param integer $customer_id
+     * @param integer $employee_id
+     * @param integer $rooms_id
      * @return mixed
      */
-    public function actionView($id)
+    public function actionView($id, $customer_id, $employee_id, $rooms_id)
     {
         return $this->render('view', [
-            'model' => $this->findModel($id),
+            'model' => $this->findModel($id, $customer_id, $employee_id, $rooms_id),
         ]);
     }
 
@@ -66,7 +69,7 @@ class BookingController extends Controller
         $model = new Booking();
 
         if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return $this->redirect(['view', 'id' => $model->ID]);
+            return $this->redirect(['view', 'id' => $model->id, 'customer_id' => $model->customer_id, 'employee_id' => $model->employee_id, 'rooms_id' => $model->rooms_id]);
         } else {
             return $this->render('create', [
                 'model' => $model,
@@ -78,14 +81,17 @@ class BookingController extends Controller
      * Updates an existing Booking model.
      * If update is successful, the browser will be redirected to the 'view' page.
      * @param integer $id
+     * @param integer $customer_id
+     * @param integer $employee_id
+     * @param integer $rooms_id
      * @return mixed
      */
-    public function actionUpdate($id)
+    public function actionUpdate($id, $customer_id, $employee_id, $rooms_id)
     {
-        $model = $this->findModel($id);
+        $model = $this->findModel($id, $customer_id, $employee_id, $rooms_id);
 
         if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return $this->redirect(['view', 'id' => $model->ID]);
+            return $this->redirect(['view', 'id' => $model->id, 'customer_id' => $model->customer_id, 'employee_id' => $model->employee_id, 'rooms_id' => $model->rooms_id]);
         } else {
             return $this->render('update', [
                 'model' => $model,
@@ -97,11 +103,14 @@ class BookingController extends Controller
      * Deletes an existing Booking model.
      * If deletion is successful, the browser will be redirected to the 'index' page.
      * @param integer $id
+     * @param integer $customer_id
+     * @param integer $employee_id
+     * @param integer $rooms_id
      * @return mixed
      */
-    public function actionDelete($id)
+    public function actionDelete($id, $customer_id, $employee_id, $rooms_id)
     {
-        $this->findModel($id)->delete();
+        $this->findModel($id, $customer_id, $employee_id, $rooms_id)->delete();
 
         return $this->redirect(['index']);
     }
@@ -110,12 +119,15 @@ class BookingController extends Controller
      * Finds the Booking model based on its primary key value.
      * If the model is not found, a 404 HTTP exception will be thrown.
      * @param integer $id
+     * @param integer $customer_id
+     * @param integer $employee_id
+     * @param integer $rooms_id
      * @return Booking the loaded model
      * @throws NotFoundHttpException if the model cannot be found
      */
-    protected function findModel($id)
+    protected function findModel($id, $customer_id, $employee_id, $rooms_id)
     {
-        if (($model = Booking::findOne($id)) !== null) {
+        if (($model = Booking::findOne(['id' => $id, 'customer_id' => $customer_id, 'employee_id' => $employee_id, 'rooms_id' => $rooms_id])) !== null) {
             return $model;
         } else {
             throw new NotFoundHttpException('The requested page does not exist.');
